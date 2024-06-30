f7b34d360c7c7eba5282136bb0358b0dbb183788
+ <+>
2024-06-29 23:53:05 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Reanza is the champion';
+document.body.appendChild(newHeader);
